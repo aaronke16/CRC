0c5b67e9b88e484a2ddd68991b24e640bf580ab5
aaronke16 <124176003+aaronke16@users.noreply.github.com>
2025-01-25 12:16:18 -0500

    more cleaning

--- index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://bpvvpt5kae47gjucw4f2dfzabu0tdonk.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `${data}`;
-    }
-
-updateCounter();
